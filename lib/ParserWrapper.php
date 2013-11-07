1f7a2fe621572fd706e65f3f503dfde7ec782a3d
espaciomore <espaciomore@gmail.com>
2013-11-07 14:03:37 -0500

    improving name and email parsing

--- lib/ParserWrapper.php
+++ lib/ParserWrapper.php
@@ -1,10 +1,13 @@
 <?php
 
+//  @author espaciomore
+
 require_once __dir__.'/MimeMailParser.class.php';
 
 class ParserWrapper
 {
     protected $parser;
+    private $headers;
 
     public function __construct()
     {
@@ -14,12 +17,20 @@ class ParserWrapper
     public function parse($mail_message)
     {
         $this->parser->setText( $mail_message );
+        $this->headers = $this->parser->getHeaders();
+        foreach ($this->headers as $key => $value) {
+            if (is_string($key)) {
+                $name = strtolower($key);
+                $this->headers[ $name ] = $value; 
+            }
+         } 
         $email = array(
             'header'=> $this->parser->getHeadersRaw(),
+            'from'=> $this->headers['from'],
             'sender_name'=> $this->get_sender_name(),
             'sender_email'=> $this->get_sender_email(),
             'sent_date'=> $this->get_sent_date(),
-            'subject'=> $this->parser->getHeader('subject'),
+            'subject'=> $this->headers['subject'],
             'content'=> $this->get_content(),
         );
         return $email;
@@ -27,19 +38,22 @@ class ParserWrapper
             
     private function get_sender_name()
     {
-        $name = trim(preg_replace('/(<.*>)+/', '', $this->parser->getHeader('from') )," \t\n\r\0\x0B\"");
+        $name = trim(preg_replace('/([^\s<@]+@[^>]+)/', '', $this->headers['from'] )," \t\n\r\0\x0B\"><");
         return $name;
     }  
     
     private function get_sender_email()
     {
-        $email = preg_replace('/[<>]+/','',preg_replace('/^.*\\s/', '', $this->parser->getHeader('from') ));
+        if ( (bool)preg_match('/([^\s<@]+@[^>]+)/', $this->headers['from'], $matches) ){
+            $email = $matches[1];
+        }
+        $email = trim($email," \t\n\r\0\x0B\"><");
         return $email;
     }  
             
     private function get_sent_date()
     {
-        $sent_date = date( 'Y-m-d H:i:s',strtotime( $this->parser->getHeader('date') ) );
+        $sent_date = date( 'Y-m-d H:i:s',strtotime( $this->headers['date'] ) );
         $sent_date = $sent_date<'1971-01-01 00:00:00' ? date('Y-m-d H:i:s',strtotime('1971-01-01 00:00:00')) : $sent_date ;
         return $sent_date;
     }

7de57ba58b4fa42b8c08145fe14809cd13bb2438
espaciomore <espaciomore@gmail.com>
2013-11-13 15:59:06 -0500

    Update ParserWrapper.php

--- lib/ParserWrapper.php
+++ lib/ParserWrapper.php
@@ -23,7 +23,10 @@ class ParserWrapper
                 $name = strtolower($key);
                 $this->headers[ $name ] = $value; 
             }
-         } 
+        }
+        if ( is_array($this->headers['from']) ) {
+            $this->headers['from'] = implode(' ', $this->headers['from']);
+        }
         $email = array(
             'header'=> $this->parser->getHeadersRaw(),
             'from'=> $this->headers['from'],
@@ -38,29 +41,45 @@ class ParserWrapper
             
     private function get_sender_name()
     {
-        $name = trim(preg_replace('/([^\s<@]+@[^\s@>]+)>?/', '', $this->headers['from'] )," \t\n\r\0\x0B\"><");
+        $regexp = '/([^\s<@]+@[^\s@>]+)>?$/';
+        $name = trim(preg_replace($regexp, '', $this->headers['from'] )," \t\n\r\0\x0B\"><");
+
         return $name;
     }  
     
     private function get_sender_email()
     {
-        if ( (bool)preg_match('/([^\s<@]+@[^\s@>]+)>?/', $this->headers['from'], $matches) ){
+        $regexp = '/([^\s<@]+@[^\s@>]+)>?/';
+        if ( (bool)preg_match($regexp, $this->headers['from'], $matches) ){
             $email = $matches[1];
+            $email = trim($email," \t\n\r\0\x0B\"><");
+        } else {
+            $email = $this->headers['from'];
         }
-        $email = trim($email," \t\n\r\0\x0B\"><");
+
         return $email;
     }  
             
     private function get_sent_date()
     {
-        $sent_date = date( 'Y-m-d H:i:s',strtotime( $this->headers['date'] ) );
-        $sent_date = $sent_date<'1971-01-01 00:00:00' ? date('Y-m-d H:i:s',strtotime('1971-01-01 00:00:00')) : $sent_date ;
+        if ( is_array($this->headers['date']) ) {
+            $this->headers['date'] = $this->headers['date'][0];
+        }
+        $filtered_strtime = preg_replace('/UT/i','UTC',$this->headers['date']);
+        $filtered_strtime = preg_replace('/\s\(.+\)/','',$this->headers['date']);
+        $filtered_strtime = preg_replace('/\s([0-9]{4})$/'," +$1",$this->headers['date']);
+        $filtered_strtime = str_replace('"','',$this->headers['date']);
+        $sent_date = date( 'Y-m-d H:i:s',strtotime( $filtered_strtime ) );
+        if ( $sent_date <= '1971-01-01 00:00:00' ){
+            $sent_date = '1971-01-01 00:00:00';
+        }
+
         return $sent_date;
     }
     
     private function get_content()
     {
-        $html = $this->parser->getMessageBody('html');
+        $html = $this->parser->getMessageBody('html',$embedded_img = true);
         $content = ($html!=='' and $html!==false) ? $html : $this->parser->getMessageBody('text');
         return $content;
     }
